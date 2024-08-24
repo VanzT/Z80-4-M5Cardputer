732386694d9f7ab6a6e9e56fdd0b1edace16050e
VanzT <102437020+VanzT@users.noreply.github.com>
2024-08-23 21:02:28 -0700

    Sparkly

--- ESP32_Z80_Emulator/serial.h
+++ ESP32_Z80_Emulator/serial.h
@@ -1,10 +1,26 @@
 #include "globals.h"
 #pragma once
 
+// Variables for biorhythm-like LED control
+int redValue = 0, greenValue = 0, blueValue = 0;
+int redIncrement = 1, greenIncrement = 2, blueIncrement = 3;
 
-//*********************************************************************************************
-//****                      Serial input and output buffer task                            ****
-//*********************************************************************************************
+// Function to update the LED colors in a biorhythm pattern
+void updateLedColor() {
+  redValue += redIncrement;
+  greenValue += greenIncrement;
+  blueValue += blueIncrement;
+
+  // Handle wraparound for 0-255 range
+  if (redValue > 255 || redValue < 0) redIncrement = -redIncrement;
+  if (greenValue > 255 || greenValue < 0) greenIncrement = -greenIncrement;
+  if (blueValue > 255 || blueValue < 0) blueIncrement = -blueIncrement;
+
+  leds[0] = CRGB(redValue, greenValue, blueValue);
+  FastLED.show();
+}
+
+// Serial input and output buffer task
 void serialTask(void *parameter) {
   char c;
   Serial.write("\n\rSerial I/O Task Started\n\r");
@@ -12,23 +28,18 @@ void serialTask(void *parameter) {
   serial_t = true;
 
   for (;;) {
-
-    //Check for chars to be sent
-    while (txOutPtr != txInPtr) {     //Have we received any chars?)
-      Serial.write(txBuf[txOutPtr]);  //Send char to console
+    // Check for chars to be sent
+    while (txOutPtr != txInPtr) {
+      Serial.write(txBuf[txOutPtr]);  // Send char to console
       if (serverClient.connected()) {
-        leds[0] = CRGB(0,0,255);
-        FastLED.show();      // Display the color
-        serverClient.write(txBuf[txOutPtr]);  //Send via Telnet if client connected
-        leds[0] = CRGB::Black;
-        FastLED.show(); 
-        //vTaskDelay(1);
+        updateLedColor();  // Update LED color in biorhythm pattern
+        serverClient.write(txBuf[txOutPtr]);  // Send via Telnet if client connected
       }
-      txOutPtr++;                                   //Inc Output buffer pointer
-      if (txOutPtr == sizeof(txBuf)) txOutPtr = 0;  //Wrap around circular buffer
+      txOutPtr++;  // Increment Output buffer pointer
+      if (txOutPtr == sizeof(txBuf)) txOutPtr = 0;  // Wrap around circular buffer
       vTaskDelay(1);
     }
-    //vTaskDelay(1);
+
     // Check for Received chars from Serial
     while (Serial.available()) {
       rxBuf[rxInPtr] = Serial.read();
@@ -40,40 +51,30 @@ void serialTask(void *parameter) {
     // Check for Received chars from Telnet
     while (serverClient.available()) {
       c = serverClient.read();
-      // Handle different newline characters
-      if (c == '\r') {
-        // Ignore carriage return if followed by line feed
-        if (serverClient.peek() == '\n') {
-          serverClient.read();
-        }
-        c = '\r';  // Just make it a CR
-      } else if (c == 127) {
-        c = 8;  // Translate backspace
-      }
+      if (c == '\r' && serverClient.peek() == '\n') serverClient.read();
+      if (c == 127) c = 8;
       rxBuf[rxInPtr] = c;
       rxInPtr++;
       if (rxInPtr == sizeof(rxBuf)) rxInPtr = 0;
     }
 
-    // Check if the virtual UART register is empty if so and there is a char waiting then put in UART register
-    if (rxOutPtr != rxInPtr && bitRead(pIn[UART_LSR], 0) == 0) {  //Have we received any chars and the read buffer is empty?
-      pIn[UART_PORT] = rxBuf[rxOutPtr];                           //Put char in UART port
-      rxOutPtr++;                                                 //Inc Output buffer pointer
+    // Handle virtual UART register
+    if (rxOutPtr != rxInPtr && bitRead(pIn[UART_LSR], 0) == 0) {
+      pIn[UART_PORT] = rxBuf[rxOutPtr];
+      rxOutPtr++;
       if (rxOutPtr == sizeof(rxBuf)) rxOutPtr = 0;
-      bitWrite(pIn[UART_LSR], 0, 1);  //Set bit to say char can be read
+      bitWrite(pIn[UART_LSR], 0, 1);
     }
 
     vTaskDelay(1);
   }
 }
 
-//*********************************************************************************************
-//****     Print string to output buffer, will send to serial and telnet if connected      ****
-//*********************************************************************************************
+// Print string to output buffer, will send to serial and telnet if connected
 void outString(char buf[]) {
   int i = 0;
   while (buf[i] > 0) {
-    txBuf[txInPtr] = buf[i];  //Write char to output buffer
+    txBuf[txInPtr] = buf[i];
     txInPtr++;
     i++;
     if (txInPtr == sizeof(txBuf)) txInPtr = 0;
